13b31e19c6443876c5c5a35d475df4ccd78b9b35
nmagrogan <nmagrogan@zagmail.gonzaga.edu>
2020-09-10 13:09:22 -0700

    added classes and some psuedocode for how the classes will function

---
+++ mastermind.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: false
+
+# Game board class, contains all of the rows for the game.
+def GameBoard
+  # stores a certain number of rows, the code to be guessed
+  # displays the game board
+end
+
+# Row class, each row is for each turn of the game, stores the CodeBreaker's guess and the CodeSetter's response
+def Row
+  # stores the guessed code, and code setters response
+  # display a row
+  # checks if guess is correct
+end
+
+# Code Setter class, the player that sets the code
+def CodeSetter
+  # Sets the code - randomly generated code
+end
+
+# Code Breaker class, the player who is guessing the code
+def CodeBreaker
+  # Gets user's inputed guess for the code
+end
+
+# Game class runs the game, keeps track of number of turns etc.
+def Game
+  # Initalizes the game
+  # keeps track of how many turns have gone by
+  # displays who won the game
+end
+
+# Runs an individul turn of the game
+def Turn
+  # has player make a guess
+  # checks if guess is correct
+end
